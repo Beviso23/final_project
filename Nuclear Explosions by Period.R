eb3626e3c625f7a8adf73fc32c6fa1710c00d867
Alex Alon <shoorik14@gmail.com>
2024-11-13 10:40:52 -0700

    New Data Set-Major Events

---
+++ Nuclear Explosions by Period.R
@@ -0,0 +1,37 @@
+# WWII 1945
+
+# Chinese Civil War   1945-1949
+
+# Indo-China War 1946-1954
+
+# Korean War 1950-1953
+
+#Iran Coup 1953
+
+#Cuban Revolution 1953-1959
+
+# Bay of Pigs Invasion 1961
+
+#Vietnam War 1955-1975
+
+#First Iraq Kurdish War 1961-1970
+
+#North Yemen Civil War 1962-1970
+
+#Lebanese Civil War 1975-1990
+
+#Islamist Uprising in Syria 1976-1982
+
+#Iranian Revolution 1978-1979
+
+#Soviet Afghan War 1978-1979
+
+#Iran-Iraq War 1980-1988
+
+
+#PLANS FOR GRAPH
+
+#1945 - 1954 End of WWII, Chinese Civil War, Korean War, First Indo-China War
+#1953 - 1959 Cuban Revolution
+#1955-1975 Vietnam War
+#1979-1989 Soviet-Afghan War, Lebanese Civil War, Iran-Iraq War
